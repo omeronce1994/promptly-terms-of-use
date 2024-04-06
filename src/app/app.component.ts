78070fa2b635dc45c71a219a47f1b4e3180b2504
omercohen <omeronce1994@gmail.com>
2024-04-07 01:23:54 +0300

    Added terms of use for promptly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+export interface TermsPlacholders {
+  appName: String
+  contactEmail: String,
+  jurisdiction: String
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +16,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'promptly-terms-of-use';
+
+  placeHolders: TermsPlacholders = {
+    appName: 'PromptBooster',
+    contactEmail: 'omeronce1995@gmail.com',
+    jurisdiction: 'Israel'
+  }
 }
